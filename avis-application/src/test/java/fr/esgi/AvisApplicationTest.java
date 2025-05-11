7b0fcdd7714453ca4921b6f731f25cb4b1bf9500
cerensec <carlosceren2807@gmail.com>
2025-05-11 23:44:26 +0200

    ajout TU avis-application

---
+++ avis-application/src/test/java/fr/esgi/AvisApplicationTest.java
@@ -0,0 +1,11 @@
+package fr.esgi;
+
+import org.junit.jupiter.api.Test;
+
+class AvisApplicationTest {
+
+    @Test
+    void testMain() {
+        AvisApplication.main(new String[]{"args"});
+    }
+}
